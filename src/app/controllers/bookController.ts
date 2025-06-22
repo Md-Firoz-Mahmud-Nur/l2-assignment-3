ba3aa52c7c34288d2a91540bb8fa874332a25d56
firoz.nur <firoznur5@gmail.com>
2025-06-22 08:44:48 +0600

    Post : Books

---
+++ src/app/controllers/bookController.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from "express";
+import { Book } from "../models/bookModel";
+
+export const bookRoutes = express.Router();
+
+bookRoutes.post(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body = req.body;
+      const book = await Book.create(body);
+
+      res.status(200).json({
+        success: true,
+        message: "Book created successfully",
+        data: book,
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
---
+++ src/app/models/bookModel.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/bookInterface";
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: [true, "title field is required"] },
+    author: { type: String, required: [true, "author field is required"] },
+    genre: {
+      type: String,
+      required: [true, "genere field is required"],
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+    },
+    isbn: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: { type: String },
+    copies: {
+      type: Number,
+      required: [true, "copies field is required"],
+      min: [0, "Negative value not allow"],
+    },
+    available: { type: Boolean, default: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const Book = model("Book", bookSchema);
