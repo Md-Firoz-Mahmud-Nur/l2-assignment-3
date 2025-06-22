832630913fdc061503e03dc07a334e952119df6f
firoz.nur <firoznur5@gmail.com>
2025-06-22 23:17:13 +0600

    Fix : Put Function

--- src/app/controllers/bookController.ts
+++ src/app/controllers/bookController.ts
@@ -85,23 +85,32 @@ bookRoutes.put(
       const bookId = req.params.bookId;
       const body = req.body;
 
-      const updatedBook = await Book.findByIdAndUpdate(bookId, body, {
-        new: true,
-        runValidators: true,
-      });
-
-      if (updatedBook === null) {
+      if (body.copies === 0) {
         res.status(404).json({
           success: false,
-          message: "Book not found",
+          message: "Insert at least one copy",
           data: null,
         });
       } else {
-        res.status(200).json({
-          success: true,
-          message: "Book updated successfully",
-          data: updatedBook,
+        body.available = true;
+        const updatedBook = await Book.findByIdAndUpdate(bookId, body, {
+          new: true,
+          runValidators: true,
         });
+
+        if (updatedBook === null) {
+          res.status(404).json({
+            success: false,
+            message: "Book not found",
+            data: null,
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "Book updated successfully",
+            data: updatedBook,
+          });
+        }
       }
     } catch (error: unknown) {
       next(error);
